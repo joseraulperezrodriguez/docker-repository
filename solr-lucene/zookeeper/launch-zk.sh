ed8ba9287de95cbdd6d0026c91829845227522ce
Jose Raul Perez Rodriguez <joseraul.perezrodriguez@gmail.com>
2018-12-09 16:34:17 +0100

    uploaded simple zookeeper docker image

---
+++ solr-lucene/zookeeper/launch-zk.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+chown -R "$ZK_USER" "$ZK_DATA_DIR" "$ZK_DATA_LOG_DIR" "$ZK_LOG_DIR" "$ZK_CONF_DIR"
+
+# modify the config file, just the first time
+if [[ ! -f "$ZK_CONF_DIR/zoo.cfg" ]]; then
+    CONFIG="$ZK_CONF_DIR/zoo.cfg"
+
+    echo "clientPort=$ZK_PORT" >> "$CONFIG"
+    echo "dataDir=$ZK_DATA_DIR" >> "$CONFIG"
+    echo "dataLogDir=$ZK_DATA_LOG_DIR" >> "$CONFIG"
+
+fi
+
+exec "$@"
